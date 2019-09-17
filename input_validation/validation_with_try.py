b739448454ef0a6fab293c659ad0c927a9bfed94
Michael Harmon <mwharmon1@dmacc.edu>
2019-09-16 22:14:23 -0500

    Updated main program by adding a try except block around the average call. Test for score3 negative value is now passing.

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -3,12 +3,12 @@
     Description: This program will prompt for user's name
     and 3 scores out of 100 then average the scores and
     display the name and average grade.
-    Update: Added if to check for negative value for score1 and score2
+    Update: Wrapped the average function call around a try except block
 """
 
 
 def average(score1, score2, score3):
-    if score1 <= 0 or score2 <= 0:
+    if score1 <= 0 or score2 <= 0 or score3 <= 0:
         raise ValueError
     else:
         average_score = (float(score1) + float(score2) + float(score3)) / 3
@@ -16,11 +16,16 @@ def average(score1, score2, score3):
 
 
 if __name__ == '__main__':
-    last_name = input('Enter your last name. ')
-    first_name = input('Enter your first name. ')
-    age = input('Enter your age. ')
-    score_1 = int(input('Enter 1st score out of 100: '))
-    score_2 = int(input('Enter 2nd score out of 100: '))
-    score_3 = int(input('Enter 3rd score out of 100: '))
-    average_scores = average(score_1, score_2, score_3)
-    print(last_name + ', ' + first_name + ' age ' + age + ' average grade: ' '% 5.2f' % average_scores)
+
+    try:
+        last_name = input('Enter your last name. ')
+        first_name = input('Enter your first name. ')
+        age = input('Enter your age. ')
+        score_1 = int(input('Enter 1st score out of 100: '))
+        score_2 = int(input('Enter 2nd score out of 100: '))
+        score_3 = int(input('Enter 3rd score out of 100: '))
+        average_scores = average(score_1, score_2, score_3)
+    except ValueError:
+        print("No negative scores please.")
+    else:
+        print(last_name + ', ' + first_name + ' age ' + age + ' average grade: ' '% 5.2f' % average_scores)
